279d722bbc708ce57ba27837f35c17d2986a9220
Karolis Zelvys <koralbish@gmail.com>
2024-10-07 21:03:29 +0300

    Add project files.

---
+++ FistLecture/Program.cs
@@ -0,0 +1,94 @@
+﻿
+using System.Drawing;
+
+Console.WriteLine("Hello, World!");
+
+
+// Y = 4 | X = 8
+Console.WriteLine();
+Console.WriteLine("   /\\   ");
+Console.WriteLine("  /  \\  ");
+Console.WriteLine(" /    \\ ");
+Console.WriteLine("/______\\");
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+// X = 15 | Y = 8
+Console.WriteLine("     *****     ");
+Console.WriteLine("   **     **   ");
+Console.WriteLine(" **         ** ");
+Console.WriteLine("**   Hello   **");
+Console.WriteLine("**    from   **");
+Console.WriteLine(" **   C#    ** ");
+Console.WriteLine("   **     **   ");
+Console.WriteLine("     *****     ");
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+// Y = 4 | X = 8
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("   /\\   ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("  /  \\  ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(" /    \\ ");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("/______\\");
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+// Y = 9 | X = 13
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(" / \\     / \\ ");
+Console.ForegroundColor= ConsoleColor.DarkCyan;
+Console.WriteLine("/   \\   /   \\");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("\\    \\ /    /");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(" \\         / ");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("  \\       /  ");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("   \\     /   ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("    \\   /    ");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("     \\ /     ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("      V      ");
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+string item = "Obuolys";
+int amount = 5;
+int price = 2;
+Console.WriteLine("Preke: " + item);
+Console.WriteLine("Kiekis: " + amount);
+Console.WriteLine("Kaina: " + price + " eur");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+string name = "Vardenis Pavardenis";
+int age = 28;
+string job = "Software Developer";
+string email = "johndoe@example.com";
+string number = "+1 123-456-7890";
+
+Console.WriteLine("============== VIZITINE ==============");
+Console.WriteLine("Vardas: " + name);
+Console.WriteLine("Amzius: " + age);
+Console.WriteLine("Pareigos: " + job);
+Console.WriteLine("E. Pastas: " + email);
+Console.WriteLine("Tel.: " + number);
+Console.WriteLine("======================================");
