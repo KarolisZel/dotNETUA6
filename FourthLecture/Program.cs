5c470a0c4ebfef53b4ab1a628ed4d6feb13366dc
Karolis Želvys <karolis.zelvs@gmail.com>
2024-10-10 19:50:37 +0300

    Renamed according to lecture
    Renamed project files according to lecture contents. Added fourth lecture

---
+++ FourthLecture/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is the Fourth lecture");
+Console.WriteLine();
+Console.WriteLine();
+
+/// TEORIJA
+//===============================================================//
+/// Switch cases
