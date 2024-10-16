cc3c3d3a17b118c757fa1b370693d38e20ed3736
Karolis Želvys <karolis.zelvs@gmail.com>
2024-10-16 20:16:15 +0300

    first two exercises done

--- SeventhLecture_Methods/Program.cs
+++ SeventhLecture_Methods/Program.cs
@@ -9,8 +9,86 @@ class Program
         Console.WriteLine("This is the Seventh lecture");
         Console.WriteLine();
         Console.WriteLine();
+        //===============================================================//
+        /// UŽDUOTIS NR.1
+
+        Console.Write("Įveskite slaptažodį: ");
+        var password = Console.ReadLine();
+        Console.WriteLine($"Ar tinkamas slaptažodis? {IsPasswordValid(password)}");
+
         //----------------------------------------------------------------//
+
+        Console.Write("Įveskite el. pašto adresą: ");
+        var email = Console.ReadLine();
+        Console.WriteLine($"Ar {email} tinkamas? {IsEmailValid(email)}");
+
+        //----------------------------------------------------------------//
+
+        Console.Write("Įveskite sumą doleriais: ");
+        var dollars = ParseDouble(Console.ReadLine());
+        if (dollars == -1)
+            Console.WriteLine("Suma buvo neįvesta arba neteisinga!");
+        else
+            Console.WriteLine($"Paversti {dollars} į Eurus: {ConvertToEuros(dollars)}");
+
+        //----------------------------------------------------------------//
+        /// UŽDUOTIS NR.2
+
+        Console.Write("Įveskite vardą: ");
+        var firstName = Console.ReadLine();
+        Console.Write("Įveskite pavardę: ");
+        var lastName = Console.ReadLine();
+        Console.WriteLine($"Inicialai: {GetInitials(firstName, lastName)}");
+
+        //----------------------------------------------------------------//
+
+        Console.Write("Įveskite spindulio dydį:");
+        var radius = ParseDouble(Console.ReadLine());
+
+        if (radius == -1)
+            Console.WriteLine("Dydis buvo neįvestas arba neteisingas!");
+
+        Console.Write("Įveskite aukštį: ");
+        var height = ParseDouble(Console.ReadLine());
+
+        if (height == -1)
+            Console.WriteLine("Dydis buvo neįvestas arba neteisingas!");
+
+        Console.WriteLine($"Cilindro tūris: {CalcutateCylinderVolume(radius, height)}");
+
+        //----------------------------------------------------------------//
+
+        Console.Write("Įveskite skaičių: ");
+        var number = ParseInt(Console.ReadLine());
+        if (number == -1)
+            Console.WriteLine("Skaičius buvo neįvestas arba neteisingas!");
+        else
+            Console.WriteLine($"Ar {number} lyginis? {IsNumberEven(number)}");
+
+        //----------------------------------------------------------------//
+
+        Console.Write("Įveskite ilgį: ");
+        var length = ParseDouble(Console.ReadLine());
+        if (length == -1)
+            Console.WriteLine("Ilgis buvo neįvestas arba neteisingas!");
+
+        Console.Write("Įveskite plotį: ");
+        var width = ParseDouble(Console.ReadLine());
+        if (width == -1)
+            Console.WriteLine("Plotas buvo neįvestas arba neteisingas!");
+
+        Console.WriteLine($"Stačiakampio plotas: {CalculateRectangleArea(length, width)}");
+
+        //----------------------------------------------------------------//
+        /// UŽDUOTIS NR.3
+
+        //----------------------------------------------------------------//
+        /// UŽDUOTIS NR.4
+
+        //----------------------------------------------------------------//
+        /// UŽDUOTIS NR.5
     }
+
     /// TEORIJA
     //===============================================================//
     /// METHODS
@@ -19,5 +97,101 @@ class Program
 
     /// PRAKTITA
     //===============================================================//
-    /// Uzduotis nr.1
+    /// HELPER METHODS
+
+    private static double ParseDouble(string input)
+    {
+        if (!string.IsNullOrEmpty(input))
+        {
+            if (double.TryParse(input, out var result))
+                return result;
+            return -1;
+        }
+        return -1;
+    }
+
+    private static int ParseInt(string input)
+    {
+        if (!string.IsNullOrEmpty(input))
+        {
+            if (int.TryParse(input, out var result))
+                return result;
+            return -1;
+        }
+        return -1;
+    }
+
+    /// Užduotis nr.1
+
+    private static bool IsPasswordValid(string password)
+    {
+        if (!string.IsNullOrEmpty(password))
+        {
+            if (password.Length < 8)
+                return false;
+
+            return true;
+        }
+
+        Console.WriteLine("Slaptažodis neįvestas!");
+        return false;
+    }
+
+    private static bool IsEmailValid(string email)
+    {
+        if (!string.IsNullOrEmpty(email))
+        {
+            if (email.Contains("@") && email.Contains("."))
+                return true;
+
+            return false;
+        }
+
+        Console.WriteLine("Neivestas el. pašto adresas!");
+        return false;
+    }
+
+    private static double ConvertToEuros(double dollars)
+    {
+        return dollars * 0.85;
+    }
+
+    //===============================================================//
+    /// Užduotis nr.2
+
+    private static string GetInitials(string firstName, string lastName)
+    {
+        return $"{firstName} {lastName}";
+    }
+
+    private static double CalcutateCylinderVolume(double radius, double height)
+    {
+        return Math.Round(Math.PI * radius * radius * height, 3);
+    }
+
+    private static bool IsNumberEven(int number)
+    {
+        if (number % 2 == 0)
+            return true;
+
+        return false;
+    }
+
+    private static double CalculateRectangleArea(double length, double width)
+    {
+        return length * width;
+    }
+
+    //===============================================================//
+    /// Užduotis nr.3
+
+
+
+    //===============================================================//
+    /// Užduotis nr.4
+
+
+
+    //===============================================================//
+    /// Užduotis nr.5
 }
