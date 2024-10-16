55049205cd226a27e0e0fb90b329d15cb948d7fa
Karolis Želvys <karolis.zelvs@gmail.com>
2024-10-16 18:49:52 +0300

    Start of lecture seven

---
+++ SeventhLecture_Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable InvalidXmlDocComment
+namespace SeventhLecture_Methods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("This is the Seventh lecture");
+        Console.WriteLine();
+        Console.WriteLine();
+        //----------------------------------------------------------------//
+    }
+    /// TEORIJA
+    //===============================================================//
+    /// METHODS
+
+
+
+    /// PRAKTITA
+    //===============================================================//
+    /// Uzduotis nr.1
+}
